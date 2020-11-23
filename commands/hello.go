4db1d874c07eb6cec44cefdb808b6c8e315dc59f
Siva <Sivas@jfrog.com>
2020-11-23 12:03:54 -0500

    Migrated code

--- commands/hello.go
+++ commands/hello.go
@@ -3,6 +3,10 @@ package commands
 import (
 	"errors"
 	"github.com/jfrog/jfrog-cli-core/plugins/components"
+	"github.com/jfrog/jfrog-cli-core/utils/config"
+	"github.com/jfrog/jfrog-client-go/artifactory/services/utils"
+	"github.com/jfrog/jfrog-client-go/httpclient"
+	clientutils "github.com/jfrog/jfrog-client-go/utils"
 	"github.com/jfrog/jfrog-client-go/utils/log"
 	"os"
 	"strconv"
@@ -64,7 +68,57 @@ type helloConfiguration struct {
 	prefix    string
 }
 
+type scanConfiguration struct {
+	componentId string
+}
+
 func helloCmd(c *components.Context) error {
+	if len(c.Arguments) != 1 {
+		return errors.New("Wrong number of arguments. Expected: 1, " + "Received: " + strconv.Itoa(len(c.Arguments)))
+	}
+	var conf = new(scanConfiguration)
+	conf.componentId = c.Arguments[0]
+	// validate format
+	artDetails, err := config.GetArtifactorySpecificConfig("", true, true)
+	if err != nil {
+		return  err
+	}
+
+	artAuth, err := artDetails.CreateArtAuthConfig()
+
+	log.Output(artAuth)
+	//rtDetails, err :=config.NewConfigBuilder().SetServiceDetails(ddtails).SetCertificatesPath(ddtails.GetClientCertPath()).Build()
+
+
+	url, err := utils.BuildArtifactoryUrl(strings.ReplaceAll(artAuth.GetUrl(),"/artifactory/","/xray/"),
+		"api/v1/summary/component", nil)
+	if err != nil {
+		return err
+	}
+	httpClientDetails := artAuth.CreateHttpClientDetails()
+
+	client, err := httpclient.ClientBuilder().Build()
+	if err != nil{
+		return err
+	}
+	httpClientDetails.Headers = map[string]string{
+		"Content-Type":"application/json",
+	}
+
+
+	_, body, err := client.SendPost(url, []byte("{\"component_details\":[{\"component_id\":\"" + conf.componentId+"\"}]}"),httpClientDetails)
+
+
+
+	if err != nil {
+		return err
+	}
+	log.Output(clientutils.IndentJson(body))
+	return nil
+}
+
+
+func helloCmd2(c *components.Context) error {
 	if len(c.Arguments) != 1 {
 		return errors.New("Wrong number of arguments. Expected: 1, " + "Received: " + strconv.Itoa(len(c.Arguments)))
 	}
